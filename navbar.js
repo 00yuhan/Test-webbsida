3f7c43fa169edd674f8d07d231248c542b91fe3c
00yuhan <97879769+00yuhan@users.noreply.github.com>
2026-03-11 23:34:18 +0100

    update hero text

--- navbar.js
+++ navbar.js
@@ -1,4 +1,30 @@
-// New navbar.js
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.nav-list');
+    const navItems = document.querySelectorAll('.nav-list a');
+    const body = document.body; // Target the body for scroll locking
+
+    if (menu && menuLinks) {
+        menu.addEventListener('click', function () {
+            menuLinks.classList.toggle('active');
+            menu.classList.toggle('is-active');
+            // Toggle scroll lock
+            body.classList.toggle('no-scroll');
+        });
+
+        navItems.forEach(link => {
+            link.addEventListener('click', () => {
+                menuLinks.classList.remove('active');
+                menu.classList.remove('is-active');
+                // Ensure scroll is restored when clicking a link
+                body.classList.remove('no-scroll');
+            });
+        });
+    }
+});
+
+
+/*
 document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('#mobile-menu');
     const menuLinks = document.querySelector('.nav-list');
@@ -18,3 +44,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+*/
